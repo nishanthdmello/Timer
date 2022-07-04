14431acc1d44f830edf2da2a195af1224f78b59f
Nishanth <106912949+nishanthdmello@users.noreply.github.com>
2022-07-04 16:29:22 +0530

    many changes
    added sound file
    deleted unrequired lines
    prgm runs properly

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,2 @@
-+s==0:
-    #     h,m,s,cs=check(0,0,0,0)
-    #     for i in range(3):
-    #         e[i].delete(0,END) 
-    #     inse
+
+        h=0
--- timer1.py
+++ timer1.py
@@ -1,20 +1,15 @@
 from tkinter import *
-# from pygame import *
-# mixer.init()
-
-
-
-# global it;it=0 
+from pygame import *
+mixer.init()
 
 def insert(*time):        
     for i in range(3):
-            e[i].insert(0,time[i])       
+            e[i].insert(0,time[i])  
 
 def get():
-    
-    #adds 00 where no input is given
+
     try:
-        h=int(e1.get())                 
+        h=int(e1.get())
     except:
         e1.insert(0,0)
         h=0
@@ -28,18 +23,19 @@ def get():
     except:
         e3.insert(0,0)
         s=0
-        
+
     for i in range(3):
             e[i].delete(0,END)
 
     if h<0 or m<0 or s<0:#makes -ve inputs zero
-        insert('00','00','00')
-        global a,it;a=0;it=0  
-    else:
-        h,m,s,cs=check(h,m,s,0)
-        insert(h,m,s)
-        # for i in range(3):
-        #     e[i].delete(0,END)                
+        insert(0,0,0)
+        h=0;m=0;s=0
+        global a;a=0
+    h=s//36000; s%=36000
+    m=s//60;    s%=60
+        
+    h,m,s,cs=check(h,m,s,0)
+    insert(h,m,s)
     return(int(h),int(m),int(s))       
 
 def check(h,m,s,cs):#makes single digit double by concatenating 0
@@ -66,41 +62,27 @@ def update():
         h,m,s,cs=check(h,m,s,cs) 
         l.config(text=f"{h}:{m}:{s}.{cs}")          
         it-=1;     
-        l.after(10,update)
+        l.after(9,update)
 
     elif it==0 and a:
         
         b1['state']=DISABLED
         l.config(text="Timer Done")            
-        # mixer.music.load("sound.mp3")
-        # mixer.music.play(loops=100)            
+        mixer.music.load("sound.mp3")
+        mixer.music.play(loops=100)            
 
 def start():
 
-    # global it,a;a=1    
     global a;a=1    
-    # h,m,s=get()
-        
-    # if h>=0 and m>=0 and s>=0:
     b1.config(command=stop,image=img[1])            
-    # for i in range(3):
-    #     e[i]['state']=DISABLED          
-    # for i in but:
-    #     i['state']=DISABLED
     update()  
 
 def initstart():
 
-    global it,a;a=1    
+    global it,a,inp;a=1
     h,m,s=get()
-    it=(h*3600+m*60+s)*100 
-    # if h+m+s==0:
-    #     h,m,s,cs=check(0,0,0,0)
-    #     for i in range(3):
-    #         e[i].delete(0,END) 
-    #     insert(h,m,s)
-        
-    if h>0 and m>0 and s>0:
+    if h>0 or m>0 or s>0:
+        it=(h*3600+m*60+s)*100
         b1.config(command=stop,image=img[1])            
         for i in range(3):
             e[i]['state']=DISABLED          
@@ -116,7 +98,7 @@ def reset():
     for i in range(3):
         e[i]['state']=NORMAL      
     b1.config(command=initstart,state=NORMAL,image=img[0])        
-    # mixer.music.stop()        
+    mixer.music.stop()        
     l.config(text="00:00:00.00")  
 
 #All declarations are here
